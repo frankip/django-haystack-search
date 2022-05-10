1e216fbed2dbd0844983e7f616a3a65f6103ac78
Francis <gwadafrank@gmail.com>
2022-05-10 23:56:01 +0300

    Fetch and display users on the templates

--- users/views.py
+++ users/views.py
@@ -2,13 +2,15 @@ from django.shortcuts import render, HttpResponse
 from django.views.generic import ListView
 # Create your views here.
 
-from ..init_db import fetch_data_from_sql
+import init_db
+
+# from .. import fetch_data_from_sql
 
 class IndexView(ListView):
     def get(self, request):
         msg = 'Welcome to the Shit Show'
         
-        users = fetch_data_from_sql()
+        users = init_db.fetch_data_from_sql()
         
         ctx = {
             "message":msg,
